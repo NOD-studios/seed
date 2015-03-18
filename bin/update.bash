03290cc338ceda460d5e9ea942a4aaf406570095
Furkan Tunalı <furkan@nod.st>
2015-03-18 17:56:28 +0200

    Updated at 18-03-2015 17:56:27

--- bin/update.bash
+++ bin/update.bash
@@ -11,7 +11,7 @@ composer selfupdate && composer update;
 echo "Updating require.js config with bower main paths";
 "$(npm bin)/bower-requirejs" -c js/main.js;
 echo "Compressing require.js";
-"$(npm bin)/uglifyjs" bower_components/requirejs/require.js --output="vendor/requirejs/require.min.js";
+"$(npm bin)/uglifyjs" vendor/requirejs/require.js --output="vendor/requirejs/require.min.js";
 echo "Building with require.js";
 "$(npm bin)/r.js" -o js/build.js;
 DATE=$(date +%d-%m-%Y" "%H:%M:%S);
