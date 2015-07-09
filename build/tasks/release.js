c75d0c4ffeeedee9c1dd92162e0088067426ab2c
Furkan Tunalı <furkan@nod.st>
2015-07-09 16:12:53 +0300

    chore(config): package configurations

--- build/tasks/release.js
+++ build/tasks/release.js
@@ -1,12 +1,32 @@
 import gulp from 'gulp';
+import env from '../env';
 import args from '../args';
 import paths from '../paths';
 import runSequence from 'run-sequence';
+import dashify from 'dashify';
 import LoadPlugins from 'gulp-load-plugins';
 import childProcess from 'child_process';
 
 const plugins = new LoadPlugins();
 const exec = childProcess.exec;
+
+gulp.task('config', () => {
+  return gulp
+    .src('./package.json')
+    .pipe(plugins.jsonEditor({
+      'name'        : dashify(env.INFO_NAME.toLowerCase()),
+      'description' : env.INFO_DESCRIPTION,
+      'repository'  : env.GIT_REPO
+    }))
+    .pipe(gulp.dest("./"));
+});
+
+gulp.task('config-sync', () => {
+  gulp
+    .src(['./bower.json', './composer.json', './package.json'])
+    .pipe(plugins.configSync())
+    .pipe(gulp.dest('./'));
+});
 
 // utilizes the bump plugin to bump the
 // semver for the repo
@@ -37,6 +57,7 @@ gulp.task('prepare', (callback) => {
     'build',
     'doc',
     'bump-version',
+    'config-sync',
     'repo-add',
     'repo-tag',
     'repo-commit',
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,10 @@
 import env from './build/env';
 import requireDir from 'require-dir';
+import gulp from 'gulp';
+import LoadPlugins from 'gulp-load-plugins';
+
+const plugins = LoadPlugins();
 
 requireDir(`./${env.DIR_BUILD}/${env.DIR_TASK}`);
+
+plugins.npmScriptSync(gulp);
