c296d0f5a1c43bd390776814db99e40e2640a388
Furkan Tunalı <furkan@nod.st>
2015-07-14 18:35:37 +0300

    style(gulp): indentation etc

--- build/tasks/update.js
+++ build/tasks/update.js
@@ -18,26 +18,25 @@ gulp.task('update-npm', (callback) => {
 
 gulp.task('update-jspm', (callback) => {
   let options = args.options || '';
-  return exec(`jspm update ${options}`,
-    (error, stdout, stderror) => {
-      console.log(stdout);
-      console.log(stderror);
-      callback(error);
-    });
+  return exec(`jspm update ${options}`, (error, stdout, stderror) => {
+    console.log(stdout);
+    console.log(stderror);
+    callback(error);
+  });
 });
 
 gulp.task('update-composer', (callback) => {
   let options = args.options || '';
-  return exec(`composer update ${options}`,
-    (error, stdout, stderror) => {
-      console.log(stdout);
-      console.log(stderror);
-      callback(error);
-    });
+  return exec(`composer update ${options}`, (error, stdout, stderror) => {
+    console.log(stdout);
+    console.log(stderror);
+    callback(error);
+  });
 });
 
 gulp.task('update', (callback) => {
   return runSequence(
-    ['update-composer', 'update-jspm', 'update-npm', 'repo-pull'], callback
+    ['update-composer', 'update-jspm', 'update-npm', 'repo-pull'],
+    callback
   );
 });
