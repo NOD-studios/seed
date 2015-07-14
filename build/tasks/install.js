9d99f33152e579090d2ec53fc302becbb1a6b66d
Furkan Tunalı <furkan@nod.st>
2015-07-14 18:34:46 +0300

    style(gulp): indentation

--- build/tasks/install.js
+++ build/tasks/install.js
@@ -20,32 +20,29 @@ gulp.task('postinstall', ['install-jspm'], (callback) => {
 
 gulp.task('install-npm', (callback) => {
   let options = args.options || '';
-  return exec(`npm install ${options}`,
-    (error, stdout, stderror) => {
-      console.log(stdout);
-      console.log(stderror);
-      callback(error);
-    });
+  return exec(`npm install ${options}`, (error, stdout, stderror) => {
+    console.log(stdout);
+    console.log(stderror);
+    callback(error);
+  });
 });
 
 gulp.task('install-jspm', (callback) => {
   let options = args.options || '';
-  return exec(`jspm install ${options}`,
-    (error, stdout, stderror) => {
-      console.log(stdout);
-      console.log(stderror);
-      callback(error);
-    });
+  return exec(`jspm install ${options}`, (error, stdout, stderror) => {
+    console.log(stdout);
+    console.log(stderror);
+    callback(error);
+  });
 });
 
 gulp.task('install-composer', (callback) => {
   let options = args.options || '';
-  return exec(`composer install ${options}`,
-    (error, stdout, stderror) => {
-      console.log(stdout);
-      console.log(stderror);
-      callback(error);
-    });
+  return exec(`composer install ${options}`, (error, stdout, stderror) => {
+    console.log(stdout);
+    console.log(stderror);
+    callback(error);
+  });
 });
 
 gulp.task('install', (callback) => {
